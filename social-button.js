9fcb3c636dec95aa1f2f1f6e34a1988a8f57541f
diegomac <dijavaji@gmail.com>
2024-11-28 13:25:47 -0500

    cambio ejecucion de submit

--- social-button.js
+++ social-button.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
   chatWidget.classList.add('chat-widget');
   chatWidget.id = 'chatWidget';
 
-  // Crear el botón flotante
+  // Crear el boton flotante
   const socialBtn = document.createElement('button');
   socialBtn.id = 'social-btn';
   socialBtn.classList.add('btn', 'btn-primary', 'shadow-lg', 'justify-content-center', 'align-items-center', 'social-btn');
 
   const chatWidgetButtonIcon = document.createElement('img');
-  chatWidgetButtonIcon.src = './public/assets/img/icon_chatbot.svg';
+  chatWidgetButtonIcon.src = 'https://raw.githubusercontent.com/Praxis-Innovacion/tec-bubble-embed/bffa0d745e7e6f70982cda04cf8b85700b3a43be/public/assets/img/icon_chatbot.svg';
   chatWidgetButtonIcon.alt = 'chat';
   chatWidgetButtonIcon.id = 'chatWidgetButtonIcon';
   chatWidgetButtonIcon.style.width = '51.82px';
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   socialBtn.appendChild(chatWidgetButtonIcon);
 
-  // Agregar el botón flotante al contenedor principal
+  // Agregar el boton flotante al contenedor principal
   chatWidget.appendChild(socialBtn);
 
   // Crear el contenedor del chat
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
   chatHeader.classList.add('chat-header');
 
   const chatHeaderIcon = document.createElement('img');
-  chatHeaderIcon.src = './public/assets/img/icon_chatbot.svg';
+  chatHeaderIcon.src = 'https://raw.githubusercontent.com/Praxis-Innovacion/tec-bubble-embed/bffa0d745e7e6f70982cda04cf8b85700b3a43be/public/assets/img/icon_chatbot.svg';
   chatHeaderIcon.alt = 'chat';
   chatHeaderIcon.id = 'chatWidgetButtonIcon';
   chatHeaderIcon.style.width = '33.12px';
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
   chatClose.id = 'chatClose';
   chatClose.innerText = '×';
 
-  // Añadir los elementos al header del chat
+  // Agrega los elementos al header del chat
   chatHeader.appendChild(chatHeaderIcon);
   chatHeader.appendChild(chatTitle);
   chatHeader.appendChild(chatClose);
@@ -80,22 +80,22 @@ document.addEventListener('DOMContentLoaded', function() {
   sendButton.type = 'submit';
 
   const sendButtonIcon = document.createElement('img');
-  sendButtonIcon.src = './public/assets/img/send10.svg';
+  sendButtonIcon.src = 'https://raw.githubusercontent.com/Praxis-Innovacion/tec-bubble-embed/bffa0d745e7e6f70982cda04cf8b85700b3a43be/public/assets/img/send10.svg';
   sendButtonIcon.alt = 'Ejecutar';
   sendButtonIcon.id = 'executeBtn';
-  sendButtonIcon.onclick = ejecutarAccion; // Asegúrate de definir `ejecutarAccion()` en tu código
+  sendButtonIcon.onclick = ejecutarAccion; //
 
   sendButton.appendChild(sendButtonIcon);
   messageInputWrapper.appendChild(messageInput);
   messageInputWrapper.appendChild(sendButton);
   chatForm.appendChild(messageInputWrapper);
 
-  // Añadir los elementos al contenedor del chat
+  // Agrega los elementos al contenedor del chat
   chatContainer.appendChild(chatHeader);
   chatContainer.appendChild(chatMessages);
   chatContainer.appendChild(chatForm);
 
-  // Añadir el chatWidget (que contiene el botón flotante y el contenedor del chat) al body
+  // Agrega el chatWidget (que contiene el boton flotante y el contenedor del chat) al body
   document.body.appendChild(chatWidget);
   document.body.appendChild(chatContainer);
 
@@ -108,13 +108,14 @@ document.addEventListener('DOMContentLoaded', function() {
       chatContainer.classList.remove('active');
   });
 
-  // Función de acción del botón "enviar"
-  function ejecutarAccion() {
-      // Lógica para el botón de enviar, como enviar el mensaje
+  // Funcion de accion del boton "enviar"
+  function ejecutarAccion(event) {
+     event.preventDefault(); // Evita que el formulario se envie
+      // Logica para el boton de enviar, como enviar el mensaje
       const messageText = messageInput.value.trim();
 
       if (messageText !== '') {
-          // Crear el nuevo mensaje y añadirlo a la vista
+          // Crear el nuevo mensaje y Agregalo a la vista
           const newMessage = document.createElement('div');
           newMessage.classList.add('message', 'user');
           newMessage.innerText = messageText;
